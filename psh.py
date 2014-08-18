ae47fb33895768199442c2ab79cfaf439ca7dd21
James Butler <jmsbtlr111@gmail.com>
2014-08-18 15:29:56 +1200

    History implemented

--- psh.py
+++ psh.py
@@ -1,13 +1,33 @@
 import os, shlex
 
+
+commandHistory = []
+commandHistoryEntryNumber = 0
+
+
 def getInput():
     return input("psh> ")
+
 
 def parseRawInput(rawInput):
     lexicalAnalyzer = shlex.shlex(rawInput, posix=True)
     lexicalAnalyzer.whitespace_split = False
     lexicalAnalyzer.wordchars += '#$+-,./?@^='
     return list(lexicalAnalyzer)
+
+
+def addToCommandHistory(rawInput):
+    global commandHistory
+    global commandHistoryEntryNumber
+    commandHistoryEntryNumber += 1
+    commandHistoryEntry = list()
+    commandHistoryEntry.append(str(commandHistoryEntryNumber))
+    commandHistoryEntry.append(rawInput)
+    if (len(commandHistory) == 10):
+        commandHistory.pop(0)
+    commandHistory.append(commandHistoryEntry)
+    return
+
 
 def parseRawCommands(commands):
     pipeIndexes = []
@@ -19,6 +39,7 @@ def parseRawCommands(commands):
     commandSequences = splitOnPipeIndexes(pipeIndexes, commands)
     return commandSequences
 
+
 def splitOnPipeIndexes(pipeIndexes, commands):
     commandSequences = []
     previousPipeIndex = 0
@@ -28,6 +49,7 @@ def splitOnPipeIndexes(pipeIndexes, commands):
         previousPipeIndex = pipeIndex + 1
     commandSequences.append(commands[previousPipeIndex:])
     return commandSequences
+
 
 def executeCommands(commandSequences):
     if (not pipesAreValid(commandSequences)):
@@ -39,6 +61,7 @@ def executeCommands(commandSequences):
         returnId = os.waitpid(processId, 0)
     return
 
+
 def pipesAreValid(commandSequences):
     if (len(commandSequences) > 1):
         for command in commandSequences:
@@ -46,6 +69,7 @@ def pipesAreValid(commandSequences):
                 print("Invalid use of pipe \"|\"")
                 return False
     return True
+
 
 def executeRecursively(commandSequences):
     if (len(commandSequences) == 1):
@@ -63,12 +87,14 @@ def executeRecursively(commandSequences):
             commandSequences.pop(0)
             executeSingleCommand(commandSequences[0])
 
+
 def setUpChildProcessReadAndWrite(r, w):
     os.close(1)
     os.dup2(w, 1)
     os.close(r)
     os.close(w)
     return
+
 
 def setUpParentProcessReadAndWrite(r, w):
     os.close(0)
@@ -77,16 +103,23 @@ def setUpParentProcessReadAndWrite(r, w):
     os.close(w)
     return
 
+
 def executeSingleCommand(command):
     if (command[0] == "pwd"):
         executePWD()
-    elif(command[0] == "cd"):
+    elif (command[0] == "cd"):
         executeCD(command)
+    elif (command[0] in ("history", "h")):
+        executeHistory(command)
+    else:
+        executeShellCommand(command)
     return
+
 
 def executePWD():
     print(os.getcwd())
     return
+
 
 def executeCD(command):
     try:
@@ -98,6 +131,24 @@ def executeCD(command):
         print(command[0] + ": " + command[1] + ": No such file or directory")
     return
 
+
+def executeHistory(command):
+    global commandHistory
+    if (len(command) == 1):
+        for commandHistoryEntry in commandHistory:
+            print(commandHistoryEntry[0] + ": " + commandHistoryEntry[1])
+    else:
+        for commandHistoryEntry in commandHistory:
+            if (command[1] == commandHistoryEntry[0]):
+                commandToBeChanged = commandHistory.pop()
+                commandToBeChanged[1] = commandHistoryEntry[1]
+                commandHistory.append(commandToBeChanged)
+                rawCommands = parseRawInput(commandHistoryEntry[1])
+                commandSequences = parseRawCommands(rawCommands)
+                executeCommands(commandSequences)
+    return
+
+
 def executeShellCommand(command):
     try:
         os.execvp(command[0], command)
@@ -105,12 +156,15 @@ def executeShellCommand(command):
         print(command[0] + ": command not found")
     return
 
+
 def main():
     while (True):
         rawInput = getInput()
+        addToCommandHistory(rawInput)
         rawCommands = parseRawInput(rawInput)
         commandSequences = parseRawCommands(rawCommands)
         executeCommands(commandSequences)
+
 
 if (__name__ == "__main__"):
     main()
